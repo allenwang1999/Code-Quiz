732b8d0316b8584ea1797d021d2999afc970f70b
Allen Wang <ayw10864@gmail.com>
2022-12-11 22:57:55 -0500

    all other requirements

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Code-Quiz
-Challenge 4 for the FSF Web Development bootcamp.
+Challenge 4 for the FSF Web Development bootcamp, which involved creating a coding quiz with a leaderboard.
+
+## Description
+The problem in Challenge 4 is to create a quiz about coding in JavaScript for users to engage with. The quiz also needed to have a leaderboard for users to be able to see after submitting there score or by pressing a button on the page.
+
+## Preview 
+![Challenge Four Preview](./Assets/04-preview.png)
+
+## Usage
+To view the webpage, go to https://allenwang1999.github.io/Code-Quiz using your browser and click the start quiz button to start the quiz.
+
+## License
+MIT License; refer to <a href="LICENSE">LICENSE</a> file in the repo.
--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 var landingElements = document.querySelector('#landing-contents');
 var highscoreButton = document.querySelector('#highscore-button');
+var quizElements = document.querySelector('#quiz-contents')
+var endElements = document.querySelector('#end-contents');
+var scoreElements = document.querySelector('#score-contents');
 var startButton = document.querySelector('#start-button');
+var endButton = document.querySelector('#name-submit');
+var nameInput = document.querySelector('#name-input');
 var time = document.querySelector('#timer');
 
-var question = 0;
+var question = document.querySelector('#question');
+var answer1 = document.querySelector('#a1');
+var answer2 = document.querySelector('#a2');
+var answer3 = document.querySelector('#a3');
+var answer4 = document.querySelector('#a4');
+var result = document.querySelector('#result');
+
+var questionNum = 0;
 var correctAnswers = 0;
 var timer;
 var timerSeconds;
 
 // todo: write elements for populate, add storage and ending screens
 //Questions taken/inspired from w3schools js quiz because I'm not creative enough https://www.w3schools.com/quiztest/quiztest.asp?qtest=JS
-var questions = {
-    question1: {
+var questions = [
+    {
         question: 'Inside which HTML element do we put JavaScript?',
         answer1: '<script>',
         answer2: '<js>',
@@ -19,7 +31,7 @@ var questions = {
         answer4: '<scripting>',
         correctAnswer: '<script>'
     },
-    question2: {
+    {
         question: 'What is the correct JavaScript syntax to change the content of the HTML element?\n<p id="demo">This is a demo</p>',
         answer1: 'document.getElementById("demo").innerHTML = "Hello World!";',
         answer2: 'document.getElement("p").innerHTML = "Hello World!";',
@@ -27,7 +39,7 @@ var questions = {
         answer4: 'document.getElementByName("p").innerHTML = "Hello World!";',
         correctAnswer: 'document.getElementById("demo").innerHTML = "Hello World!";'
     },
-    question3: {
+    {
         question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
         answer1: '<script src="xxx.js">',
         answer2: '<script href="xxx.js">',
@@ -35,7 +47,7 @@ var questions = {
         answer4: '<script class="xxx.js">',
         correctAnswer: '<script src="xxx.js">'
     },
-    question4: {
+    {
         question: 'How do you write "Hello World" in an alert box?',
         answer1: 'msgBox("Hello World");',
         answer2: 'msg("Hello World");',
@@ -43,7 +55,7 @@ var questions = {
         answer4: 'alertBox("Hello World");',
         correctAnswer: 'alert("Hello World");'
     },
-    question5: {
+    {
         question: 'How do you create a function in JavaScript?',
         answer1: 'function = myFunction()',
         answer2: 'function:myFunction()',
@@ -51,7 +63,7 @@ var questions = {
         answer4: 'def function()',
         correctAnswer: 'function myFunction()'
     },
-    question6: {
+    {
         question: 'How do you call a function named "myFunction"?',
         answer1: 'myFunction()',
         answer2: 'myFunction',
@@ -59,7 +71,7 @@ var questions = {
         answer4: 'call myFunction()',
         correctAnswer: 'myFunction()'
     },
-    question7: {
+    {
         question: 'How to write an IF statement in JavaScript?',
         answer1: 'if i = 5',
         answer2: 'if i = 5 then',
@@ -67,7 +79,7 @@ var questions = {
         answer4: 'if (i == 5)',
         correctAnswer: 'if (i == 5)'
     },
-    question8: {
+    {
         question: 'What is a proper way to write a WHILE loop?',
         answer1: 'while(i = 0; i < 5; i++)',
         answer2: 'while (i < 5; i++)',
@@ -75,7 +87,7 @@ var questions = {
         answer4: 'while i < 5',
         correctAnswer: 'while(i < 5)'
     },
-    question9: {
+    {
         question: 'Which of these is a JavaScript Comment?',
         answer1: '`This is a comment`',
         answer2: '||This is a comment||',
@@ -83,7 +95,7 @@ var questions = {
         answer4: '//This is a comment',
         correctAnswer: '//This is a comment'
     },
-    question10: {
+    {
         question: 'What would be the output of "console.log(0 == false)"?',
         answer1: 'true',
         answer2: 'false',
@@ -91,25 +103,117 @@ var questions = {
         answer4: 'null',
         correctAnswer: 'true'
     },
-}
+]
 
 highscoreButton.addEventListener("click", showHighscores);
 startButton.addEventListener("click", startQuiz);
+answer1.addEventListener("click", answerClick);
+answer2.addEventListener("click", answerClick);
+answer3.addEventListener("click", answerClick);
+answer4.addEventListener("click", answerClick);
+endButton.addEventListener("click", submitScore);
 
-function showHighscores (e) {
-    console.log("highscore button clicked");
+function submitScore(e) {
+    var scoreName = nameInput.value;
+    var score = correctAnswers + timerSeconds;
+    var highScore = {scoreName, score};
+    var highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+    highScores.push(highScore);
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    
+    
+    showHighscores();
 }
 
-function startQuiz (e) {
-    console.log("start button clicked");
-    landingElements.style.visibility = 'hidden';
+function answerClick(e) {
+    if (this.textContent == questions[questionNum].correctAnswer) {
+        correctAnswers++;
+        questionNum++;
+        result.textContent = 'Correct!';
+        populate();
+    } else {
+        timerSeconds -= 5;
+        questionNum++;
+        result.textContent = 'Incorrect.';
+        populate();
+    }
+}
+
+function showHighscores(e) {
+    var highScores = JSON.parse(localStorage.getItem('highScores') || "[]");
+    var placement = 1;
+    console.log("highscore button clicked");
+    console.log(`Highscores: ${highScores}`);
+    quizElements.style.display = 'none';
+    landingElements.style.display = 'none';
+    endElements.style.display = 'none';
+    scoreElements.style.display = 'flex';
+
+    var header = document.createElement('h1');
+    header.textContent = 'High Scores';
+    scoreElements.append(header);
+
+    var container = document.createElement('div');
+    container.setAttribute('id', 'highscore-container');
+    for(let i = 0; i < highScores.length; i++) {
+        var entry = document.createElement('h2');
+        entry.setAttribute('class', 'highScore');
+        entry.textContent = `${placement}. ${highScores[i].scoreName} ${highScores[i].score}`;
+        container.append(entry);
+        placement++;
+    }
+    scoreElements.append(container);
+
+    var buttonRow = document.createElement('div');
+    buttonRow.setAttribute('class', 'row');
+
+    var backButton = document.createElement('button');
+    backButton.textContent = 'Back To Start';
+    backButton.addEventListener('click', (e) => {
+        scoreElements.style.display = 'none';
+        landingElements.style.display = 'flex';
+        while (scoreElements.lastChild) {
+            scoreElements.removeChild(scoreElements.lastChild);
+        }
+    });
+    buttonRow.append(backButton);
+
+    var clearButton = document.createElement('button');
+    clearButton.textContent = 'Clear Highscores';
+    clearButton.addEventListener('click', (e) => {
+        var highScoreContainer = document.querySelector('#highscore-container');
+        highScoreContainer.remove();
+        localStorage.clear();
+    })
+    buttonRow.append(clearButton);
+    scoreElements.append(buttonRow);
+
+    questionNum = 0;
     correctAnswers = 0;
-    timerSeconds = 10;
+}
+
+function startQuiz(e) {
+    console.log("start button clicked");
+    landingElements.style.display = 'none';
+    quizElements.style.display = 'flex';
+    correctAnswers = 0;
+    timerSeconds = 60;
     startTimer();
     populate();
 }
 
 function populate() {
+    if (questionNum == 10) {
+        endGame();
+    }
+    else {
+        var questionContents = questions[questionNum];
+        question.textContent = questionContents.question;
+        answer1.textContent = questionContents.answer1;
+        answer2.textContent = questionContents.answer2;
+        answer3.textContent = questionContents.answer3;
+        answer4.textContent = questionContents.answer4;
+    }
 
 }
 
@@ -117,8 +221,7 @@ function startTimer() {
     timer = setInterval(function () {
         timerSeconds--;
         time.textContent = timerSeconds;
-        if(timerCount <= 0) {
-            clearInterval(timer);
+        if (timerSeconds <= 0) {
             endGame();
         }
     }, 1000)
@@ -126,4 +229,6 @@ function startTimer() {
 
 function endGame() {
     clearTimeout(timer);
+    quizElements.style.display = 'none';
+    endElements.style.display = 'flex';
 }
